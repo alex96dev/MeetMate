c9f17f7e12d3cf2fed1570a70adb0b2100ddb179
Stellan Wetzig <stellanwetzig@yahoo.de>
2024-03-05 16:36:48 +0100

    Add all features of user story 4 - edit activity

--- components/DetailsCard/index.js
+++ components/DetailsCard/index.js
@@ -2,12 +2,20 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import Link from "next/link";
+import CardForm from "../CardForm";
+import { useState } from "react";
 
 export default function DetailsCard() {
   const router = useRouter();
   const { id } = router.query;
 
-  const { data: activities, isLoading } = useSWR(`/api/activities/${id}`);
+  const {
+    data: activities,
+    isLoading,
+    mutate,
+  } = useSWR(`/api/activities/${id}`);
+
+  const [isEditMode, setIsEditMode] = useState(false);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -15,6 +23,25 @@ export default function DetailsCard() {
 
   if (!activities) {
     return;
+  }
+
+  async function handleEditActivity(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const activityData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/activities/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(activityData),
+    });
+    if (response.ok) {
+      mutate();
+      setIsEditMode(false);
+      event.target.reset();
+    }
   }
 
   return (
@@ -30,9 +57,21 @@ export default function DetailsCard() {
       {activities.description !== "" && (
         <p>description: {activities.description}</p>
       )}
-      <Link href="/">
-        <StyledCloseButton>Close</StyledCloseButton>
-      </Link>
+      <StyledButtonBox>
+        <Link href="/">
+          <StyledCloseButton>Close</StyledCloseButton>
+        </Link>
+        <StyledEditButton
+          type="button"
+          onClick={() => {
+            setIsEditMode(!isEditMode);
+          }}
+        >
+          {" "}
+          {isEditMode ? "Cancel Edit" : "Edit Activity"}
+        </StyledEditButton>
+      </StyledButtonBox>
+      {isEditMode && <CardForm onSubmit={handleEditActivity} />}
     </StyledDetailsCard>
   );
 }
@@ -49,4 +88,11 @@ const StyledDetailsCard = styled.div`
   border-radius: 15px;
 `;
 
+const StyledButtonBox = styled.div`
+  display: flex;
+  gap: 2rem;
+`;
+
 const StyledCloseButton = styled.button``;
+
+const StyledEditButton = styled.button``;
