366ea3f649b2963f8b1608cd922d91fe93020aab
Timur Sakalli <the.timur.and+github@gmail.com>
2024-03-19 13:32:19 +0100

    add searchbar

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,34 @@
+import React, { useState } from "react";
+import styled from "styled-components";
 import ActivityCard from "@/components/ActivityCard";
 import useSWR from "swr";
 import Link from "next/link";
-import styled from "styled-components";
 import PlaceholderLogo from "@/components/Logo/placeholder";
 import Navigation from "@/components/Navigation";
 import { theme } from "@/styles";
+import SearchBar from "/components/SearchBar";
 
 export default function HomePage() {
   const { data: activities, isLoading } = useSWR("/api/activities");
+  const [filteredActivities, setFilteredActivities] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (searchTerm) => {
+    setSearchTerm(searchTerm);
+    if (!searchTerm) {
+      setFilteredActivities([]);
+      return;
+    }
+    const filtered = activities.filter((activity) =>
+      activity.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredActivities(filtered);
+  };
 
   if (isLoading) return <div>loading...</div>;
   if (!activities) return <div>failed to load</div>;
+
+  let displayedActivities = searchTerm ? filteredActivities : activities;
 
   return (
     <>
@@ -18,18 +36,34 @@ export default function HomePage() {
         <PlaceholderLogo />
         <h1>MeetMate</h1>
       </StyledHeadlineBox>
+      <SearchBar onSearch={handleSearch} />
       <StyledCardSection>
-        {activities.map((activity) => (
-          <Link key={activity._id} href={`/${activity._id}`}>
-            <ActivityCard
-              name={activity.name}
-              date={activity.date}
-              time={activity.time}
-              joined={activity.joined}
-              category={activity.category}
-            />
-          </Link>
-        ))}
+        {searchTerm === "" &&
+          activities.length > 0 &&
+          activities.map((activity) => (
+            <Link key={activity._id} href={`/${activity._id}`}>
+              <ActivityCard
+                name={activity.name}
+                date={activity.date}
+                time={activity.time}
+                joined={activity.joined}
+                category={activity.category}
+              />
+            </Link>
+          ))}
+        {displayedActivities.length > 0
+          ? displayedActivities.map((activity) => (
+              <Link key={activity._id} href={`/${activity._id}`}>
+                <ActivityCard
+                  name={activity.name}
+                  date={activity.date}
+                  time={activity.time}
+                  joined={activity.joined}
+                  category={activity.category}
+                />
+              </Link>
+            ))
+          : searchTerm !== "" && <div>No results found</div>}
       </StyledCardSection>
       <Navigation />
     </>
